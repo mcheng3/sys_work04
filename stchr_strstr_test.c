d82747217e196b35a6e7e488a391243f12b7f0ca
mcheng3 <michael.cheng65@gmail.com>
2017-09-28 21:03:01 -0400

    Finish stchr_strstr.c

--- stchr_strstr_test.c
+++ stchr_strstr_test.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-int main{
+int main(){
+	//strchr returns pointer of given character in given string; 0 if not found
+	char s[] = "Hello!";
+	char c = 'e';
 	
+	printf("s: \"%s\"\n", s);
+	printf("c: \'%c\'\n", c);
+	printf("strchr(s, c): %p\n", strchr(s, c));
+	printf("strchr(s, c): %s\n\n", strchr(s, c));
+
+	//strstr returns pointer of the first occurance of a given substring in a given string
+	//0 if not found
+
+	char sub[] = "lo!";
+
+	printf("sub: \"%s\"\n", sub);
+	printf("strstr(s, sub): %p\n", strstr(s, sub));
+	printf("strstr(s, sub): %s\n", strstr(s, sub));
+
 	return 0;
 }
